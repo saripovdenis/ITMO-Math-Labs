75b259663ff6237a811ea0f51f4b9769a8b25420
ni-k-azakov <nakazakov@edu.hse.ru>
2021-03-27 03:15:14 +0300

    :sparkles: Update: Fibonacci v1 add

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,11 +1,11 @@
 import Methods/Dichotomy
 import Methods/Parabola
 import Methods/GoldenRatio
-
+import Methods/Fibonacci
 print("---Algorithm---")
 pritn("1. Dichotomy")
 pritn("2. GoldenRatio")
-pritn("3. Fibonacci (not implemented)")
+pritn("3. Fibonacci")
 pritn("4. Parabola")
 pritn("5. Brent (not implemented)")
 print("---------------")
@@ -17,6 +17,8 @@ if var == 1:
     Dichotomy.run()
 elif var == 2:
     GoldenRatio.run()
+elif var == 3:
+    Fibonacci.run()
 elif var == 4:
     Parabola.run()
 else:
---
+++ Lab1/Methods/Fibonacci.py
@@ -0,0 +1,49 @@
+import numpy as np
+def run():
+    print("Left border: ")
+    left_border = int(input())
+    print("Right border: ")
+    right_border = int(input())
+    print("ε:")
+    e = float(input())
+    n = 1
+
+    y = lambda x: np.sin(x) - np.log(x * x) + 10
+    fib = lambda m: 1 / np.sqrt(5) * (((1 + np.sqrt(5)) / 2) ** m - ((1 - np.sqrt(5)) / 2) ** m)
+    while fib(n) <= (right_border - left_border) / e:
+        n += 1
+
+    x_1 = left_border + fib(n - 2) / fib(n) * (right_border - left_border)
+    x_2 = left_border + fib(n - 1) / fib(n) * (right_border - left_border)
+
+    x_1_value = y(x_1)
+    x_2_value = y(x_2)
+
+    prev_x_1 = 0
+    for i in range(1, n - 1):
+        prev_x_1 = x_1
+        if x_1_value > x_2_value:
+            left_border = x_1
+            x_1 = x_2
+            x_2 = left_border + fib(n - i - 1) / fib(n - i) * (right_border - left_border)
+            x_1_value = x_2_value
+            x_2_value = y(x_2)
+        else:
+            right_border = x_2
+            x_2 = x_1
+            x_1 = left_border + fib(n - i - 1) / fib(n - i) * (right_border - left_border)
+            x_2_value = x_1_value
+            x_1_value = y(x_1)
+        print(x_1)
+        print(x_2)
+        print(x_1_value)
+        print(x_2_value)
+        print()
+        print(left_border)
+        print(right_border)
+        print()
+    minimum = (left_border + right_border) / 2
+    print(f"Iter amount: {n}")
+    print(f"Minimum: {minimum}")
+    print(f"Minimum value: {y(minimum)}")
+
