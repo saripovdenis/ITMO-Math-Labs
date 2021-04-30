cd0e3fc4c982e7650d625fc4cb246dc52a2b7730
ni-k-azakov <nani.l.desu.l.ka@gmail.com>
2021-04-30 22:05:09 +0300

    gradient

--- Lab2/Main.py
+++ Lab2/Main.py
@@ -1,3 +1,6 @@
+import gradient_method as gm
+
 fun = lambda x: 2 * x ** 2 + 3 * x + 3
 der_fun = lambda x: 4 * x + 3
-print("Lab2")
+
+gm.gradient_method(fun, der_fun, 0.1, 40)
--- Lab2/gradient_method.py
+++ Lab2/gradient_method.py
@@ -1,2 +1,6 @@
-def gradient_method(step, iter_amount):
-    print("mem")
+def gradient_method(fun, der_fun, step, iter_amount):
+    prev_x = 0
+    for i in range(iter_amount):
+        temp_x = prev_x - step * der_fun(prev_x)
+        prev_x = temp_x
+    print(f'x: {temp_x}, y: {fun(temp_x)}')
