e556e76bd20efdc3408693c5dc607394a9eb67a3
Nikita <55149626+ni-k-azakov@users.noreply.github.com>
2021-05-13 18:18:45 +0300

    Create Task_1.py

---
+++ Lab3/Task_1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def ul_rasfleksovka(matrix):
+    n = len(matrix)
+    u = np.array(matrix)
+    l = np.zeros((n, n))
+
+
+    for i in range(n):
+        for j in range(i, n):
+            l[j][i] = u[j][i] / u[i][i]
+
+    for k in range(1, n):
+        for i in range(k - 1, n):
+            for j in range(i, n):
+                l[j][i] = u[j][i] / u[i][i]
+        for i in range(k, n):
+            for j in range(k - 1, n):
+                u[i][j] = u[i][j] - l[i][k - 1] * u[k - 1][j]
+    print(u)
+    print(l)
