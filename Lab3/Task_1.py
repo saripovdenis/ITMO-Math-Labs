6180f9af63a085703ef585d5d806ad1ecff566f0
Nikita <55149626+ni-k-azakov@users.noreply.github.com>
2021-05-14 19:08:52 +0300

    :lipstick: Update: structure enhance

--- Lab3/Task_1.py
+++ Lab3/Task_1.py
@@ -1,7 +1,7 @@
 import numpy as np
 from obrat_l import reverse
 from scipy import sparse as sp
-
+from matrixZipper import zipEm
 
 def get_lu(matrix):
     n = len(matrix)
@@ -29,10 +29,14 @@ def find(l, u, b):
     # l * y = b
     y = reverse(l).dot(b)
     x = reverse(u.T).T.dot(y)
-    print(x)
+    return x
 
 
-def task_1(data_a, index_a, ind_ptr_a, data_b, index_b, ind_ptr_b, data_e, index_e, ind_ptr_e):
+def find_x(data_a, index_a, ind_ptr_a, data_b, index_b, ind_ptr_b):
     lu = get_lu(sp.csr_matrix((data_a, index_a, ind_ptr_a)).toarray())  # lu
-    find(np.array(lu[0]), np.array(lu[1]), sp.csr_matrix((data_b, index_b, ind_ptr_b)).toarray())  # x
-    find(np.array(lu[0]), np.array(lu[1]), sp.csr_matrix((data_e, index_e, ind_ptr_e)).toarray())  # a^-1
+    return zipEm(find(np.array(lu[0]), np.array(lu[1]), sp.csr_matrix((data_b, index_b, ind_ptr_b)).toarray()))  # x
+
+
+def find_a_obrat(data_a, index_a, ind_ptr_a, data_e, index_e, ind_ptr_e):
+    lu = get_lu(sp.csr_matrix((data_a, index_a, ind_ptr_a)).toarray())  # lu
+    return zipEm(find(np.array(lu[0]), np.array(lu[1]), sp.csr_matrix((data_e, index_e, ind_ptr_e)).toarray()))  # a^-1
