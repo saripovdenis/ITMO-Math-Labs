3438872ed2b23a91bc1887581942b89bc16886f2
bblankbboi <bblankbboi@gmail.com>
2021-05-14 23:21:57 +0500

    Add index counter

--- Lab3/GaussSeidelMethod.py
+++ Lab3/GaussSeidelMethod.py
@@ -3,11 +3,13 @@ from matrixZipper import zipEm
 from scipy import sparse as sp
 
 def seidel(A, b, eps):
+    iterations = 0
     n = len(A)
     x = np.zeros(n)  # zero vector
 
     converge = False
     while not converge:
+        iterations += 1
         x_new = np.copy(x)
         for i in range(n):
             s1 = sum(A[i][j] * x_new[j] for j in range(i))
@@ -17,7 +19,7 @@ def seidel(A, b, eps):
         converge = np.sqrt(sum((x_new[i] - x[i]) ** 2 for i in range(n))) <= eps
         x = x_new
 
-    return x
+    return x, iterations
 
 def arch_seidel(data_a, index_a, ind_ptr_a, data_b, index_b, ind_ptr_b, eps):
     A = sp.csr_matrix((data_a, index_a, ind_ptr_a)).toarray()
