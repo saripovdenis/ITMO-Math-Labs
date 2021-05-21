cadc7cb456e9801426c69329b3df1223f1203708
Nikita <55149626+ni-k-azakov@users.noreply.github.com>
2021-05-21 23:04:31 +0300

    :bug: Fix: Task_6 bug fix

--- Lab3/Task_6.py
+++ Lab3/Task_6.py
@@ -1,7 +1,7 @@
 import time
 from diagonalDominantMatrix import diagonalDominantMatrix
 from matrixZipper import zipEm
-from Task_1.py import find_x as uniter_method
+from Task_1 import find_x
 from GaussSeidelMethod import seidel as iter_method
 
 def task6():
@@ -13,10 +13,10 @@ def task6():
         matrix = diagonalDominantMatrix(size)
 
         #check uniteration method
-        [data, index, ind_ptr] = zipEm(matrix)
+        data, index, ind_ptr = zipEm(matrix)
 
         uniter_start_time = time.time()
-        uniter_method(data, index, ind_ptr)
+        find_x(data, index, ind_ptr)
 
         duration = time.time() - uniter_start_time
 
@@ -33,6 +33,6 @@ def task6():
     
     return result
 
-
+print(task6())
 
 
